80038de93f6bdf0cc72a6494b713c72ce4fe382a
Khalidsaour78 <148840364+Khalidsaour78@users.noreply.github.com>
2024-02-26 03:21:18 -0800

    changing character appearance with direction moved

--- index.js
+++ index.js
@@ -2,53 +2,24 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
-move(character).to(100, 250)
+//making the character appearance change with direction moved.
+if(direction === null){
+    character.src = 'assets/green-character/static.gif'
+}
+if(direction === 'west'){
+    character.src = 'assets/green-character/west.gif'
+}
+if(direction === 'north'){
+    character.src = 'assets/green-character/north.gif'
+}
+if(direction === 'east'){
+    character.src = 'assets/green-character/east.gif'
+}
+if(direction === 'south'){
+    character.src = 'assets/green-character/south.gif'
+}
 
-//Making the character stop moving when  ArrowKey is not pressed
-document.addEventListener('keyup', function(e){
-    direction = null
-})
-
-//Changing character's direction with arrow keys
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-
-//Defining the moveCharacter callback function inline 
-setInterval (function (){
-	if(direction === 'west'){
-		x = x - 1
-	}
-	if(direction === 'north'){
-		y = y + 1
-	}
-	if(direction === 'east'){
-		x = x + 1
-	}
-	if(direction === 'south'){
-		y = y - 1
-	}
-	character.style.left = x + 'px'
-	character.style.bottom = y + 'px'
-}, 1)
-
+move(character).withArrowKeys(100, 250)
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
